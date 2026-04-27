8696c6855515ce2aacbf61bb54e03c05fa223219
antonin-lfv <antoninlefevre45@icloud.com>
2026-04-27 17:57:35 +0200

    change port - env usage

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ tailwindcss -c /app/tailwind.config.js \
     -i /app/app/static/css/input.css \
     -o /app/app/static/css/tailwind.css \
     --minify 2>&1 | grep -v "Browserslist"
-exec uv run uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+exec .venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
