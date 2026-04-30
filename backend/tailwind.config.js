3ca38c81cfd9686902b0f8e34d5e33770228cac0
antonin-lfv <antoninlefevre45@icloud.com>
2026-05-01 00:58:38 +0200

    TVA et TTC auto rempli, amélioration du flux

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/templates/**/*.html'],
+  safelist: [
+    'text-emerald-500',
+  ],
   theme: {
     extend: {
       fontFamily: {
